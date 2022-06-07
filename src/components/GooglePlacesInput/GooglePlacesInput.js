a4a2c2295e33ebaf41619eb601d2ee1e0a651cd8
zoabitech <smartcompany95@gmail.com>
2022-06-08 00:03:13 +0300

    searchcomp

--- src/components/GooglePlacesInput/GooglePlacesInput.js
+++ src/components/GooglePlacesInput/GooglePlacesInput.js
@@ -1,64 +1,35 @@
 import React from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
 const GooglePlacesInput = () => {
     return (
-        <GooglePlacesAutocomplete
-            placeholder='Search'
-            onPress={(data, details = null) => {
-                // 'details' is provided when fetchDetails = true
-                console.log(data, details);
-            }}
-            query={{
-                key: 'YOUR API KEY',
-                language: 'en',
-            }}
-        />
+        <View style={styles.container}>
+            <GooglePlacesAutocomplete
+                placeholder="Search"
+                query={{
+                    key: 'AIzaSyCTdxFZgp-AY6YpRcLYHtCnnCCLF5UOn-E',
+                    language: 'en', // language of the results
+                }}
+                onPress={(data, details = null) => console.log(data)}
+                onFail={(error) => console.error(error)}
+                fetchDetails={true}
+            // requestUrl={{
+            //   url:
+            //     'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api',
+            //   useOnPlatform: 'web',
+            // }} // this in only required for use on the web. See https://git.io/JflFv more for details.
+            />
+        </View>
     );
 };
-
 export default GooglePlacesInput;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 10,
+        // paddingTop: Constants.statusBarHeight + 10,
+        backgroundColor: '#ecf0f1',
     },
-    textInputContainer: {
-        flexDirection: 'row',
-    },
-    textInput: {
-        backgroundColor: '#FFFFFF',
-        height: 44,
-        borderRadius: 5,
-        paddingVertical: 5,
-        paddingHorizontal: 10,
-        fontSize: 15,
-        flex: 1,
-    },
-    poweredContainer: {
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        borderBottomRightRadius: 5,
-        borderBottomLeftRadius: 5,
-        borderColor: '#c8c7cc',
-        borderTopWidth: 0.5,
-    },
-    powered: {},
-    listView: {},
-    row: {
-        backgroundColor: '#FFFFFF',
-        padding: 13,
-        height: 44,
-        flexDirection: 'row',
-    },
-    separator: {
-        height: 0.5,
-        backgroundColor: '#c8c7cc',
-    },
-    description: {},
-    loader: {
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        height: 20,
-    },
-})
+});
